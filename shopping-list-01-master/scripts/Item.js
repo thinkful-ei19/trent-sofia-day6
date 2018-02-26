f9bdf432841ac0ddc5b78ea4fd8d2bb1162776ed
trentduncan <trent.dunc@gmail.com>
2018-02-26 11:36:50 -0800

    refactored methods into store

--- shopping-list-01-master/scripts/Item.js
+++ shopping-list-01-master/scripts/Item.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const Item = (function (){
     function validateName(name){
         if (!name) {
--- shopping-list-01-master/scripts/store.js
+++ shopping-list-01-master/scripts/store.js
@@ -1,3 +1,5 @@
+/* global cuid */
+
 const store = (function () { 
     const items = [
           { id: cuid(), name: 'apples', checked: false },
@@ -9,10 +11,45 @@ const store = (function () {
     
     let searchTerm = '';
     
+    function findById(id){
+      return store.items.find(item => item.id === id);
+    }
+
+    function addItem(name) {
+        try {
+            Item.validateName(name);
+            this.items.push(Item.create(name));
+        } catch (error) {
+            console.log('Cannot add item: ' + error.message);
+        }
+    }
+
+    function findAndToggleChecked(id) {
+        return this.findById(id).checked = !this.findById(id).checked;
+    }
+
+    function findAndUpdateName(id, newName) {
+        try {
+            Item.validateName(name);
+            this.findById(id).name = newName;
+        } catch (error) {
+            console.log('Cannot update name: ' + error.message);
+        }
+    }
+
+    function findAndDelete(id) {
+        const filteredList = this.items.filter(item => item.id !== this.findById(id).id);
+        this.items = filteredList;
+    }
     
     return {
         items,
         hideCheckedItems,
-        searchTerm
+        searchTerm,
+        findById,
+        addItem,
+        findAndToggleChecked,
+        findAndUpdateName,
+        findAndDelete
     }
 }());
