2437892a5195c27bdb4b0e59b2f9ee576421b50b
trentduncan <trent.dunc@gmail.com>
2018-02-26 09:28:10 -0800

    finished step 2 in namespacing

--- shopping-list-01-master/scripts/Item.js
+++ shopping-list-01-master/scripts/Item.js
@@ -1,7 +1,21 @@
 const Item = (function (){
+    function validateName(name){
+        if (!name) {
+            throw new TypeError('Name does not exist');
+        }
+    }
 
+    function create(name) {
+        return {
+            id: cuid(),
+            name,
+            checked: false
+        }
+    }
+    
     return {
-
+        validateName,
+        create
     }
 
 }());
